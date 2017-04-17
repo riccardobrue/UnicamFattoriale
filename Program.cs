c46c8453476bee75aefaef9b6c57c370984726c4
Riccardo Bruè <riccardo.brue@gmail.com>
2017-04-18 01:01:29 +0200

    resolved a no-number insertion

--- Program.cs
+++ Program.cs
@@ -8,20 +8,36 @@ namespace UnicamFattoriale
         {
             //Stampo dei messaggi di cortesia per l'utente, così lo guido all'utilizzo del programma
             Console.WriteLine("Questo programma calcola il fattoriale di un numero intero");
-            Console.Write("Digita un numero intero e premi invio: ");
 
-            //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
-            //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
-            string testoDigitatoDallUtente = Console.ReadLine();
+            int numero = 0;
+            while (true)
+            {
+                Console.Write("Digita un numero intero e premi invio: ");
+                //Console.ReadLine si mette in attesa che l'utente digiti qualcosa e prema invio.
+                //Quando ha premuto invio, il testo che ha digitato lo assegno ad una variabile di tipo string.
+                string testoDigitatoDallUtente = Console.ReadLine();
 
-            //Interpreto il testo come un numero intero
-            //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
-            //che non è interpretabile come un numero! (Es. ABCD)
-            int numero = int.Parse(testoDigitatoDallUtente);
+                //Interpreto il testo come un numero intero
+                //Attenzione: questa istruzione fallirà se l'utente ha digitato un testo
+                //che non è interpretabile come un numero! (Es. ABCD)
+
+                try
+                {
+                    numero = int.Parse(testoDigitatoDallUtente);
+                    break;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("You have inserted a NON-Number");
+
+                }
+            }
 
             //Calcolo il fattoriale: qui ci sono degli errori!
             int risultato = 0;
-            while (numero >= 0) {
+            while (numero >= 0)
+            {
                 risultato *= numero;
                 numero--;
             }
